ee1960399718d77c29a6b1b8ccc54c7b76de717d
KongoDaModder <132005160+KongoDaModder@users.noreply.github.com>
2023-08-28 02:12:51 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.Kongo.gorillatag.TakeADive";
+		public const string Name = "Take A Dive";
+		public const string Version = "1.0.0";
+	}
+}
